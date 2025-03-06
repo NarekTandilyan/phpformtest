2d0c8d592bf3f0e00ffb6a99a17606223579a2cc
NarekTandilyan <narektandilian@mail.ru>
2025-03-06 17:41:33 +0900

    Add files via upload

---
+++ send.php
@@ -0,0 +1,39 @@
+<?php
+
+    $name = $_POST['name'];
+    $tel = $_POST['tel'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $name = htmlspecialchars($name);
+    $tel = htmlspecialchars($tel);
+    $email = htmlspecialchars($email);
+    $message = htmlspecialchars($message);
+
+    $name = urldecode($name);
+    $tel = urldecode($tel);
+    $email = urldecode($email);
+    $message = urldecode($message);
+
+    $name = trim($name);
+    $tel = trim($tel);
+    $email = trim($email);
+    $message = trim($message);
+
+
+    if(mail('narektandilian@mail.ru',
+            'Nuevo mensaje',
+            'Nombre: '.$name."\n".
+            'Tel: '.$tel."\n".
+            'Email: '.$email."\n".
+            'message: '.$message."\n",
+            'From: crisple.info \r\n'
+            )
+    ){
+        echo ('OK');
+    }
+    else{
+        echo('Error..');
+    }
+    
+?>
