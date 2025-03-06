b76134d8aecb48acf71d7e35d20f09ef4c983bca
NarekTandilyan <narektandilian@mail.ru>
2025-03-06 17:40:00 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<form action="send.php" method="post">
+	<span id="span_name">Nombre</span>
+	<input type="text" name="name" id="name">
+
+	<span id="span_tel">Número de teléfono</span>
+	<input type="tel" name="tel" id="tel">
+
+	<span id="span_email">Correo electrónico</span>
+	<input type="email" name="email" id="email">
+
+	<span id="span_text">Mensaje</span>
+	<textarea placeholder="" name="message" id="text"></textarea>
+	<br>
+	<input type="submit" value="Enviar" name="btn_submit" id="submit">
+</form>
